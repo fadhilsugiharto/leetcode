7ddac2ee1d61e66e290b9a5d5bb0fcd0f1849f69
Fadhil Sugiharto <fadhilsugiharto@gmail.com>
2023-06-08 14:25:11 +0700

    3. Longest Substring Without Repeating Characters (sliding window techique)

--- LongestSubstringWithoutRepeatingCharacters.go
+++ LongestSubstringWithoutRepeatingCharacters.go
@@ -2,38 +2,59 @@ package main
 
 import "fmt"
 
-func lengthOfLongestSubstring(s string) int {
-	if s == "" {
-		return 0
-	}
-	MaxCount := 1
-	temp := ""
-	check := true
+//func lengthOfLongestSubstring(s string) int {
+//	if s == "" {
+//		return 0
+//	}
+//	MaxCount := 1
+//	temp := ""
+//	check := true
+//
+//	for i, r := range s {
+//		temp = string(r)
+//		for j := i + 1; j < len(s); j++ {
+//			for _, t := range temp {
+//				if string(s[j]) == string(t) {
+//					temp = ""
+//					check = false
+//					break
+//				}
+//			}
+//			if check == false {
+//				check = true
+//				break
+//			}
+//			temp += string(s[j])
+//			if MaxCount < len(temp) {
+//				MaxCount = len(temp)
+//			}
+//		}
+//	}
+//
+//	return MaxCount
+//}
 
-	for i, r := range s {
-		temp = string(r)
-		for j := i + 1; j < len(s); j++ {
-			for _, t := range temp {
-				if string(s[j]) == string(t) {
-					temp = ""
-					check = false
-					break
-				}
-			}
-			if check == false {
-				check = true
-				break
-			}
-			temp += string(s[j])
-			if MaxCount < len(temp) {
-				MaxCount = len(temp)
+func lengthOfLongestSubstring(s string) int {
+	dict := [128]bool{}
+	count := 0
+	max := 0
+
+	for i, j := 0, 0; i < len(s); i++ {
+		if dict[s[i]] == true {
+			for ; dict[s[i]]; j++ {
+				count--
+				dict[s[j]] = false
 			}
 		}
+		dict[s[i]] = true
+		count++
+		if max < count {
+			max = count
+		}
 	}
-
-	return MaxCount
+	return max
 }
 
 func main() {
-	fmt.Println(lengthOfLongestSubstring("pwwkew"))
+	fmt.Println(lengthOfLongestSubstring("bbbbbbb"))
 }
