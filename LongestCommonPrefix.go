97a679f389a2b312e1e5aa22a55dfc20f77b83a0
Fadhil Sugiharto <fadhilsugiharto@gmail.com>
2023-06-09 18:49:49 +0700

    14. Longest Common Prefix  (Shorten code, worse runtime)

--- LongestCommonPrefix.go
+++ LongestCommonPrefix.go
@@ -2,29 +2,43 @@ package main
 
 import "fmt"
 
-func longestCommonPrefix(strs []string) string {
-	prefix := ""
-	first := strs[0]
+//func longestCommonPrefix(strs []string) string {
+//	prefix := ""
+//	first := strs[0]
+//
+//	for i := 0; i < len(first); i++ {
+//		checker := ""
+//		for i2, str := range strs {
+//			if i2 == 0 {
+//				checker = string(str[i])
+//			} else {
+//				if len(str) == i || string(str[i]) != checker {
+//					return prefix
+//				}
+//			}
+//		}
+//		prefix += string(first[i])
+//	}
+//	return prefix
+//}
 
-	for i := 0; i < len(first); i++ {
-		checker := ""
-		for i2, str := range strs {
-			if i2 == 0 {
-				checker = string(str[i])
-			} else {
-				if len(str) == i || string(str[i]) != checker {
-					return prefix
-				}
-			}
+func longestCommonPrefix(strs []string) string {
+	p := strs[0]
+	for _, str := range strs {
+		i := 0
+		if str == p {
+			continue
 		}
-		prefix += string(first[i])
+		for ; len(str) > i && len(p) > i && str[i] == p[i]; i++ {
+		}
+		p = p[:i]
 	}
-	return prefix
+	return p
 }
 
 func main() {
 	strs := []string{
-		"ab", "a",
+		"a", "abcd",
 	}
 	fmt.Println(longestCommonPrefix(strs))
 }
