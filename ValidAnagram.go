3a7d0ac12a216902869f891fff0b94ef29723be2
Fadhil Sugiharto <fadhilsugiharto@gmail.com>
2023-06-11 20:20:09 +0700

    242. Valid Anagram (sort string and compare)

--- ValidAnagram.go
+++ ValidAnagram.go
@@ -3,20 +3,42 @@ package main
 import (
 	"fmt"
 	"reflect"
+	"sort"
 )
 
+// WITH MAP
+//func isAnagram(s string, t string) bool {
+//	sMap := map[string]int{}
+//	for i := 0; i < len(s); i++ {
+//		sMap[string(s[i])] += 1
+//	}
+//	tMap := map[string]int{}
+//	for i := 0; i < len(t); i++ {
+//		tMap[string(t[i])] += 1
+//	}
+//	return reflect.DeepEqual(sMap, tMap)
+//}
+
 func isAnagram(s string, t string) bool {
-	sMap := map[string]int{}
-	for i := 0; i < len(s); i++ {
-		sMap[string(s[i])] += 1
-	}
-	tMap := map[string]int{}
-	for i := 0; i < len(t); i++ {
-		tMap[string(t[i])] += 1
-	}
-	return reflect.DeepEqual(sMap, tMap)
+	// Convert the string to a slice of runes
+	runesS := []rune(s)
+
+	// Sort the rune slice
+	sort.Slice(runesS, func(i, j int) bool {
+		return runesS[i] < runesS[j]
+	})
+
+	// Convert the string to a slice of runes
+	runesT := []rune(t)
+
+	// Sort the rune slice
+	sort.Slice(runesT, func(i, j int) bool {
+		return runesT[i] < runesT[j]
+	})
+
+	return reflect.DeepEqual(runesS, runesT)
 }
 
 func main() {
-	fmt.Println(isAnagram("anagram", "nagaram"))
+	fmt.Println(isAnagram("car", "tar"))
 }
