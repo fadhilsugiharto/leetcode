befced64b249da61298a0a643ea63d525e9d76cf
Fadhil Sugiharto <fadhilsugiharto@gmail.com>
2023-06-11 20:37:39 +0700

    242. Valid Anagram (using array, lighter than map)

--- ValidAnagram.go
+++ ValidAnagram.go
@@ -2,8 +2,6 @@ package main
 
 import (
 	"fmt"
-	"reflect"
-	"sort"
 )
 
 // WITH MAP
@@ -19,26 +17,47 @@ import (
 //	return reflect.DeepEqual(sMap, tMap)
 //}
 
+// Sort and compare
+//func isAnagram(s string, t string) bool {
+//	// Convert the string to a slice of runes
+//	runesS := []rune(s)
+//
+//	// Sort the rune slice
+//	sort.Slice(runesS, func(i, j int) bool {
+//		return runesS[i] < runesS[j]
+//	})
+//
+//	// Convert the string to a slice of runes
+//	runesT := []rune(t)
+//
+//	// Sort the rune slice
+//	sort.Slice(runesT, func(i, j int) bool {
+//		return runesT[i] < runesT[j]
+//	})
+//
+//	return reflect.DeepEqual(runesS, runesT)
+//}
+
 func isAnagram(s string, t string) bool {
-	// Convert the string to a slice of runes
-	runesS := []rune(s)
+	if len(s) != len(t) {
+		return false
 
-	// Sort the rune slice
-	sort.Slice(runesS, func(i, j int) bool {
-		return runesS[i] < runesS[j]
-	})
+	}
+	var ans [26]int
+	for i := 0; i < len(s); i++ {
+		ans[s[i]-'a']++
+		ans[t[i]-'a']--
+	}
 
-	// Convert the string to a slice of runes
-	runesT := []rune(t)
+	for _, v := range ans {
+		if v != 0 {
+			return false
+		}
+	}
 
-	// Sort the rune slice
-	sort.Slice(runesT, func(i, j int) bool {
-		return runesT[i] < runesT[j]
-	})
-
-	return reflect.DeepEqual(runesS, runesT)
+	return true
 }
 
 func main() {
-	fmt.Println(isAnagram("car", "tar"))
+	fmt.Println(isAnagram("anagram", "nagaram"))
 }
