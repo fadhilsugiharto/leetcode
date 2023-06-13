cd36cedd35a4eddd4e50c9c4b21f90b78c606137
Fadhil Sugiharto <fadhilsugiharto@gmail.com>
2023-06-13 08:27:04 +0700

    26. Remove Duplicates from Sorted Array

---
+++ RemoveDuplicatesfromSortedArray.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func removeDuplicates(nums []int) int {
+	i := 0
+	for j := 1; j < len(nums); j++ {
+		if nums[i] != nums[j] {
+			i++
+			nums[i] = nums[j]
+		}
+	}
+	return i + 1
+}
+
+func main() {
+	fmt.Println(removeDuplicates([]int{0, 0, 1, 1, 1, 2, 2, 3, 3, 4}))
+}
