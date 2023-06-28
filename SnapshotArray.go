26eefeda47d154ce6fc2b2fb16470bf999573cd4
Fadhil Sugiharto <fadhilsugiharto@gmail.com>
2023-06-28 09:01:38 +0700

    1146. Snapshot Array (better approach)

--- SnapshotArray.go
+++ SnapshotArray.go
@@ -2,44 +2,112 @@ package main
 
 import "fmt"
 
+// NGGAE DEWE
+//type SnapshotArray struct {
+//	snapshot map[int][]int
+//}
+//
+//func Constructor(length int) SnapshotArray {
+//	snapshotArray := SnapshotArray{
+//		snapshot: map[int][]int{
+//			0: make([]int, length),
+//		},
+//	}
+//	return snapshotArray
+//}
+//
+//func (this *SnapshotArray) Set(index int, val int) {
+//	recentArray := this.snapshot[len(this.snapshot)-1]
+//	if index < len(recentArray) {
+//		recentArray[index] = val
+//	}
+//}
+//
+//func (this *SnapshotArray) Snap() int {
+//	this.snapshot[len(this.snapshot)] = make([]int, len(this.snapshot[len(this.snapshot)-1]))
+//	copy(this.snapshot[len(this.snapshot)], this.snapshot[len(this.snapshot)-1])
+//
+//	return len(this.snapshot) - 2
+//}
+//
+//func (this *SnapshotArray) Get(index int, snap_id int) int {
+//	return this.snapshot[snap_id][index]
+//}
+
+// 5 Head Approach
+type Snapshot struct {
+	id  int
+	val int
+}
+
+type SnapshotsHistory struct {
+	snapshots *[]Snapshot
+	currVal   int
+}
+
 type SnapshotArray struct {
-	snapshot map[int][]int
+	array      *[]*SnapshotsHistory
+	snapshotId int
 }
 
 func Constructor(length int) SnapshotArray {
-	snapshotArray := SnapshotArray{
-		snapshot: map[int][]int{
-			0: make([]int, length),
-		},
+	arr := make([]*SnapshotsHistory, length)
+	res := SnapshotArray{&arr, 0}
+
+	for i := 0; i < length; i++ {
+		hist := make([]Snapshot, 0)
+		(*res.array)[i] = &SnapshotsHistory{&hist, 0}
 	}
-	return snapshotArray
+
+	return res
 }
 
 func (this *SnapshotArray) Set(index int, val int) {
-	recentArray := this.snapshot[len(this.snapshot)-1]
-	if index < len(recentArray) {
-		recentArray[index] = val
+	hist := (*this.array)[index]
+
+	if this.snapshotId > 0 && (len(*hist.snapshots) == 0 || (*hist.snapshots)[len(*hist.snapshots)-1].id < this.snapshotId) {
+		*hist.snapshots = append(*hist.snapshots, Snapshot{this.snapshotId - 1, hist.currVal})
 	}
+
+	hist.currVal = val
 }
 
 func (this *SnapshotArray) Snap() int {
-	this.snapshot[len(this.snapshot)] = make([]int, len(this.snapshot[len(this.snapshot)-1]))
-	copy(this.snapshot[len(this.snapshot)], this.snapshot[len(this.snapshot)-1])
-
-	return len(this.snapshot) - 2
+	defer func() {
+		this.snapshotId++
+	}()
+	return this.snapshotId
 }
 
-func (this *SnapshotArray) Get(index int, snap_id int) int {
-	return this.snapshot[snap_id][index]
-}
+func (this *SnapshotArray) Get(index int, snapId int) int {
+	hist := (*this.array)[index]
 
-/**
- * Your SnapshotArray object will be instantiated and called as such:
- * obj := Constructor(length);
- * obj.Set(index,val);
- * param_2 := obj.Snap();
- * param_3 := obj.Get(index,snap_id);
- */
+	if len(*hist.snapshots) == 0 || (*hist.snapshots)[len(*hist.snapshots)-1].id < snapId {
+		return hist.currVal
+	}
+
+	l := 0
+	r := len(*hist.snapshots)
+	m := 0
+
+	for l < r {
+		m = (l + r) / 2
+
+		mId := (*hist.snapshots)[m].id
+
+		if m == l && mId >= snapId || m > l && mId >= snapId && (*hist.snapshots)[m-1].id < snapId {
+			break
+		}
+
+		if mId < snapId {
+			l = m + 1
+		} else {
+			r = m
+		}
+	}
+
+	return (*hist.snapshots)[m].val
+}
 
 func main() {
 	arr := Constructor(3)
