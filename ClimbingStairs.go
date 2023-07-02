3990a46f8a30e2dc83c96cf5f1b0d3d086d8b173
Fadhil Sugiharto <fadhilsugiharto@gmail.com>
2023-07-02 12:42:23 +0700

    70. Climbing Stairs (best approach)

--- ClimbingStairs.go
+++ ClimbingStairs.go
@@ -2,28 +2,39 @@ package main
 
 import "fmt"
 
+//func climbStairs(n int) int {
+//	if n == 1 {
+//		return 1
+//	} else if n == 2 {
+//		return 2
+//	}
+//
+//	// list of steps needed for each staircase
+//	stairsStepCounts := make([]int, n+1)
+//	// anak tangga ne siji mung iso sak step
+//	stairsStepCounts[0] = 1
+//	// anak tangga ne 2 iso 2 step, either mletik langsung 2 langkah (langsung tekan target)
+//	// nek ora mletik pisan ning anak tangga 1, which then uses jumlah step yg dilakukan pas koe munggah nek anak tangga 1
+//	stairsStepCounts[1] = 2
+//
+//	for i := 3; i <= n; i++ {
+//		stairsStepCounts[2] = stairsStepCounts[0] + stairsStepCounts[1]
+//		stairsStepCounts[0] = stairsStepCounts[1]
+//		stairsStepCounts[1] = stairsStepCounts[2]
+//	}
+//
+//	return stairsStepCounts[2]
+//}
+
 func climbStairs(n int) int {
-	if n == 1 {
-		return 1
-	} else if n == 2 {
-		return 2
+	one, two := 1, 1
+
+	for i := 0; i < n-1; i++ {
+		temp := one
+		one = one + two
+		two = temp
 	}
-
-	// list of steps needed for each staircase
-	stairsStepCounts := make([]int, n+1)
-	// anak tangga ne siji mung iso sak step
-	stairsStepCounts[0] = 1
-	// anak tangga ne 2 iso 2 step, either mletik langsung 2 langkah (langsung tekan target)
-	// nek ora mletik pisan ning anak tangga 1, which then uses jumlah step yg dilakukan pas koe munggah nek anak tangga 1
-	stairsStepCounts[1] = 2
-
-	for i := 3; i <= n; i++ {
-		stairsStepCounts[2] = stairsStepCounts[0] + stairsStepCounts[1]
-		stairsStepCounts[0] = stairsStepCounts[1]
-		stairsStepCounts[1] = stairsStepCounts[2]
-	}
-
-	return stairsStepCounts[2]
+	return one
 }
 
 func main() {
