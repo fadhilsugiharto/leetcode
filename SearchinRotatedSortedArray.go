4a2f442f50f610ec76bcfb75d8a7ac8b11504adf
Fadhil Sugiharto <fadhilsugiharto@gmail.com>
2023-07-05 12:24:20 +0700

    33. Search in Rotated Sorted Array

--- SearchinRotatedSortedArray.go
+++ SearchinRotatedSortedArray.go
@@ -7,26 +7,24 @@ func search(nums []int, target int) int {
 	left := 0
 	right := lenNums - 1
 
-	for left >= 0 || right <= lenNums-1 {
-		middle := (left + right) / 2
+	for left <= right {
+		middle := left + (right-left)/2
 		if target == nums[middle] {
 			return middle
 		}
-		if left >= right || right < left {
-			break
-		}
-		// check if sequence is on the left
-		if nums[left] < nums[middle] {
-			if target >= nums[left] && target < nums[middle] {
-				right = middle - 1
-			} else {
+
+		// check if sequence is on the right
+		if nums[left] > nums[middle] {
+			if nums[middle] < target && target <= nums[right] {
 				left = middle + 1
+			} else {
+				right = middle - 1
 			}
-		} else /* if sequence is in the right */ {
-			if target <= nums[right] && target >= nums[middle+1] {
-				left = middle + 1
-			} else {
+		} else /* if sequence is on the left */ {
+			if nums[left] <= target && target < nums[middle] {
 				right = middle - 1
+			} else {
+				left = middle + 1
 			}
 		}
 	}
