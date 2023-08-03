eb1371789f286f061dbfe844711ef7907dccb1d7
Fadhil Sugiharto <fadhilsugiharto@gmail.com>
2023-08-03 14:52:34 +0700

    189. Rotate Array

--- RotateArray.go
+++ RotateArray.go
@@ -19,6 +19,21 @@ func rev(nums []int, start int, end int) {
 	}
 }
 
+//func rotate(nums []int, k int) {
+//	n := len(nums)
+//	k %= n
+//
+//	for i := 0; i < k; i++ {
+//		temp := nums[n-1]
+//
+//		for i := n - 1; i > 0; i-- {
+//			nums[i] = nums[i-1]
+//		}
+//		nums[0] = temp
+//	}
+//
+//}
+
 func main() {
 	nums := []int{0, 1, 2, 3, 4, 5, 6, 7}
 	rotate(nums, 3)
